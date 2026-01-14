f6a32c177ada27442f7c5938d9908e90aa7f88f5
Vlad Pavliuk <vladpavliuk@gmail.com>
2026-01-14 12:18:24 +0100

    Initial commit

---
+++ src/main/java/com/example/autodepot/AutoDepotApplication.java
@@ -0,0 +1,13 @@
+package com.example.autodepot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDepotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoDepotApplication.class, args);
+	}
+
+}
